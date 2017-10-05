f5bd86546eb468c2010665d1f064bcafab49e3d9
Lucas Leandro <lucasleandro1204@gmail.com>
2017-10-05 20:27:33 -0300

    set token

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use JWTAuth;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, CreatesUsers;
+
+    public function actingAs(Authenticatable $user, $driver = null)
+    {
+        $token = JWTAuth::fromUser($user);
+        JWTAuth::setToken($token);
+
+        return $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ]);
+    }
 }
