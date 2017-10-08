73a7f25904968368c142c10fbeaf896359386df8
Lucas Leandro <lucasleandro1204@gmail.com>
2017-10-08 15:31:46 -0300

    better

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,23 +1,20 @@
 <?php
 
-function dispatchInSequence(array $jobs, $data)
-{
-    return array_reduce($jobs, function ($data, $job) {
-        return dispatch_now(new $job($data));
-    }, $data);
-}
-
-function user()
-{
-    return auth()->user();
+if (! function_exists('user')) {
+    /**
+     * Return user authenticated.
+     */
+    function user()
+    {
+        return auth()->user();
+    }
 }
 
 if (! function_exists('filter_keys')) {
     /**
      * Filter an array and return the keys.
-     *
      */
-    function filter_keys(array $data)
+    function filter_keys(array $data): array
     {
         return array_keys(array_filter($data));
     }
